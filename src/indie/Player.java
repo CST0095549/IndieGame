c02b4a1462a15ed4d2272e35da4116f83fa77cb6
Userpl022pc19 <Userpl022pc19@SLFRPL022PC19.ensino.atec.local>
2016-11-08 11:40:15 +0000

    teste 08-11-2016

---
+++ src/indie/Player.java
@@ -0,0 +1,27 @@
+package indie;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Userpl022pc19
+ */
+public class Player {
+    
+    int fatigue;
+    int atack;
+    int defense;
+    
+    public void pass() {
+        
+    }
+    
+    public void shoot() {
+        
+    }
+    
+}
---
+++ src/indie/SensibleDummy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indie;
+
+/**
+ *
+ * @author Userpl022pc19
+ */
+public class SensibleDummy {
+    
+    String nome;
+    String cor;
+    
+    public void move(){ 
+        
+        int x;
+        int y;
+        
+    }
+    
+}
